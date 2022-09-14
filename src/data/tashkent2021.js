5bb08edce58b2092773b558823d83a7328fee542
Eanna <eannafreeney@gmail.com>
2022-09-14 17:14:19 +0200

    worked on taskkent page

---
+++ src/data/tashkent2021.js
@@ -0,0 +1,291 @@
+export const DayOne = {
+  partOne: {
+    title: "Day 1: Opening Session",
+    chair: {
+      name: "Kristine Tovmasyn",
+      link: "https://www.youtube.com/watch?v=tEjjIqcnRJA",
+      desc: "UNESCO Almaty",
+    },
+    speakers: [
+      {
+        name: "Alexandros Makarigakis",
+        link: "https://www.youtube.com/watch?v=QJ3jDmxhR28",
+        desc: "Head of Office and UNESCO Representative a.i., UNESCO Office in Tashkent.",
+      },
+      {
+        name: "Sayidafzal Mallakhanov",
+        link: "https://www.youtube.com/watch?v=kEdLrwvy9q4",
+        desc: "Deputy Secretary-General, National commission of the Republic of Uzbekistan for UNESCO",
+      },
+    ],
+  },
+  overview: {
+    title: "GLOFCA project overview, status, and meeting objectives ",
+    speakers: [
+      {
+        name: "Natalya Kim",
+        link: "https://www.youtube.com/watch?v=bxiOvjJCd6E",
+        desc: "UNESCO Almaty – short overview of the project and its objectives.",
+      },
+      {
+        name: "Simon Allen",
+        link: "https://www.youtube.com/watch?v=B8Kp1ldf9eA",
+        desc: "University of Zurich, Switzerland.",
+      },
+    ],
+  },
+  sessionOne: {
+    title:
+      "Session 1: Experiences from Kazakhstan on lake mapping, monitoring, and hazard assessment.",
+    chairs: [
+      {
+        name: "Ms Meerim Asanbaikyzy",
+        desc: "Kyrgyzhydromet",
+      },
+      {
+        name: "Ms Zhuldyz Zhurumbetova",
+        desc: "UNESCO Almaty",
+      },
+    ],
+    speakers: [
+      {
+        name: "Atay Myrzabayev",
+        link: "https://www.youtube.com/watch?v=sz5IrOowtqI",
+        desc: "Deputy Head, Department for Emergency Situations Prevention, Ministry of Emergency Situations of the Republic of Kazakhstan – warning, information, communication aspects.",
+      },
+      {
+        name: "Murat Kassenov",
+        link: "https://www.youtube.com/watch?v=fXrMWVSdvVs",
+        desc: "Deputy Head, State Agency “KAZSELEZASCHITA” – glacier lakes monitoring.",
+      },
+      {
+        name: "Nikolay Popov",
+        link: "https://www.youtube.com/watch?v=DVH8P_M3w4I",
+        desc: "Chief Scientist, Institute of Geography and Water Security – glacier lake mapping and hazard assessment.",
+      },
+      {
+        name: "Vassiliy Kapitsa",
+        link: "https://www.youtube.com/watch?v=glhtb3vCx9c",
+        desc: "Senior Researcher, Central Asian Regional Glaciological Centre category 2 under the auspices of UNESCO – glacier lake mapping and hazard assessment.",
+      },
+      {
+        name: "Bakhtiyar Ospanov",
+        link: "https://www.youtube.com/watch?v=3yUiV8Azv",
+        desc: "Chief Expert of the Center for Emergency Situations and Disaster Risk Reduction – the Center’s experience in glacier lake monitoring and risk assessment.",
+      },
+    ],
+  },
+  sessionTwo: {
+    title:
+      "Session 2: Experiences from Kyrgyzstan on lake mapping, monitoring, and hazard assessment",
+    chairs: [
+      {
+        name: "Mr Bakhtiyar Ospanov",
+        desc: "CESDRR",
+      },
+      {
+        name: "Ms Zuura Mamadalieva",
+        desc: "UNESCO Almaty",
+      },
+    ],
+    speakers: [
+      {
+        name: "Dauberk Sakyev",
+        link: "https://www.youtube.com/watch?v=b6DIZypxhGw&t=1s",
+        desc: "Head, Department of Analysis and Forecasting, Ministry of Emergency Situations of the Kyrgyz Republic – warning, information, communication aspects.",
+      },
+      {
+        name: "Bolot Moldobekov",
+        link: "https://www.youtube.com/watch?v=B37ukzBwBHA",
+        desc: "Co-director, Central Asian Institute for Applied Geosciences (CAIAG) – lake mapping, monitoring, and hazard assessment.",
+      },
+      {
+        name: "Vitalij Zaginaev",
+        link: "https://www.youtube.com/watch?v=mG342lZn-4k&t=2s",
+        desc: "Senior Researcher, Institute of Water Problems and Hydroenergy of the National Academy of Sciences of the Kyrgyz Republic – lake mapping, monitoring, and hazard assessment.",
+      },
+    ],
+  },
+  sessionThree: {
+    title:
+      "Session 3: Experiences from Tajikistan on lake mapping, monitoring, and hazard assessment (cont.)",
+    chairs: [
+      {
+        name: "Mr Kodirjon Mingboev",
+        desc: "State Committee of the Republic of Uzbekistan on Geology and Mineral Resources",
+      },
+      {
+        name: "Mr Bakhtibek Otambekzoda",
+        desc: "UNESCO Almaty",
+      },
+    ],
+    speakers: [
+      {
+        name: "Idibek Buriev",
+        link: "https://www.youtube.com/watch?v=5KHjVkPk6cw",
+        desc: "ead, Emergency Prevention and Response Department, Committee of Emergency Situations and Civil Defence of Tajikistan – warning, information, communication aspects.",
+      },
+      {
+        name: "Kamoliddin Nazirzoda",
+        link: "https://www.youtube.com/watch?v=sMqLMg95ON0",
+        desc: "Deputy Head, Center for Glaciology, Agency for Hydrometeorology, Committee of Environmental Protection under the Government of the Republic of Tajikistan – lake mapping, monitoring and hazard assessment.",
+      },
+      {
+        name: "Abdulhamid Kayumov",
+        link: "https://www.youtube.com/watch?v=CUBQdV6pX-8",
+        desc: "Director, State Scientific Institution “Center for the Glaciers Study under the Academy of Sciences of the Republic of Tajikistan” – lakes mapping/monitoring and hazard assessment.",
+      },
+      {
+        name: "Zaynura Khudoyorbekova / Mr Tohir Sabzaliev",
+        link: "https://www.youtube.com/watch?v=l-kXNi5iOA8",
+        desc: "Aga Khan Agency for Habitat (AKAH) – lake mapping, monitoring and hazard assessment.",
+      },
+    ],
+  },
+  sessionFour: {
+    title:
+      "Session 4: Experiences from Uzbekistan on lake mapping, monitoring, and hazard assessment.",
+    chairs: [
+      {
+        name: "Mr Jafar Niyazov",
+        desc: "Institute of Water Problems of Tajikistan",
+      },
+      {
+        name: "Mr Obidjon Kodirov",
+        desc: "UNESCO Almaty",
+      },
+    ],
+    speakers: [
+      {
+        name: "Khurshid Sotiev",
+        link: "https://www.youtube.com/watch?v=SwvM0u3ujVE",
+        desc: "Head, National Centre for Emergency Management and Response, Ministry of Emergency Situations of the Republic of Uzbekistan – warning, information, communication aspects.",
+      },
+      {
+        name: "Davron Azimov",
+        link: "https://www.youtube.com/watch?v=iLF50CmNfVY",
+        desc: "Head, Department for Monitoring Hazardous Hydrometeorological Observation Network, Centre of Hydrometeorological Service of the Republic of Uzbekistan (Uzhydromet) – lakes mapping/monitoring and hazard assessment.",
+      },
+      {
+        name: "Maksim Petrov",
+        link: "https://www.youtube.com/watch?v=JO6diO9dVYc",
+        desc: "Centre of Glacial Geology, Institute of Geology and Geophysics named after Kh.M. Abdullaev – lakes mapping/monitoring and hazard assessment.  ",
+      },
+      {
+        name: "Fazliddin Anarboev",
+        link: "https://www.youtube.com/watch?v=Nq7ySF-293w",
+        desc: "Deputy Director, State Monitoring Service of Uzbekistan for Geo-hazard – georisks and hazard assessment. ",
+      },
+    ],
+  },
+  sessionFive: {
+    title:
+      "Session 5: Identifying key gaps and synergies across Central Asia in lake mapping, monitoring, and hazard assessment",
+    chair: {
+      name: "Christian Huggel",
+      link: "https://www.youtube.com/watch?v=WuUSuDke6Qo",
+      desc: "University of Zurich",
+    },
+    points: [
+      "Plenary style open discussion around a series of questions posed by the Chair",
+      "Emerge with clear synthesis of common approaches being used across the countries",
+      "Emerge with clear synthesis on the gaps and needs GLOFCA project can address",
+    ],
+  },
+  sessionSix: {
+    title:
+      "Session 6: Addressing key gaps and strengthening monitoring and assessment capacities, and knowledge management",
+    chair: {
+      name: "Mr Holger Frey",
+      link: "https://www.youtube.com/watch?v=nvqtGNAW8CY",
+      desc: "University of Zurich",
+    },
+    speakers: [
+      {
+        name: "Holger Frey",
+        link: "https://www.youtube.com/watch?v=nvqtGNAW8CY",
+        desc: "Addressing key gaps and strengthening monitoring and assessment capacities, and knowledge management.",
+      },
+      {
+        name: "Alfred Diebold",
+        link: "https://www.youtube.com/watch?v=k_vrJ_bYr7w",
+        desc: "Presentation of the GLOFCA Knowledge Management Platform.",
+      },
+      {
+        name: "Simon Allen",
+        link: "https://youtu.be/Vo0e--aFI9Q",
+        desc: "Capacity building activities planned under GLOFCA project",
+      },
+      {
+        name: "Manu Tom",
+        link: "https://www.youtube.com/watch?v=66dl3uFO0nQ&t=2s",
+        desc: "GLOFCA tool box for lake monitoring ",
+      },
+    ],
+  },
+  sessionSeven: {
+    title: "Session 7: Wrap-up",
+    chair: {
+      name: "Kristine Tovmasyan",
+      link: "https://www.youtube.com/watch?v=tEjjIqcnRJA",
+      desc: "UNESCO Almaty",
+    },
+  },
+}
+
+export const DayTwo = {
+  partOne: {
+    title: "Day 2: Working Group discussions",
+    chair: {
+      name: "Ms Kristine Tovmasyan",
+      desc: "UNESCO Almaty",
+    },
+    points: [
+      "Introduction from the moderator (10 min)",
+      "Parallel group discussions at country level and a youth discussion on DRR, facilitated by UZH team, Country Coordinators and Disaster and Climate Resilience Youth Network (DACRYN) (90 min)",
+    ],
+  },
+  partTwo: {
+    title: "Working group discussions (cont.)",
+    chair: {
+      name: "UZH team",
+      desc: "Country Coordinators, DACRYN",
+    },
+    points: [
+      "Wrap-up and overview of the outcomes of the group discussions (7 min per group)",
+    ],
+  },
+  partThree: {
+    title: "Closing session",
+    chair: {
+      name: "Ms Kristine Tovmasyan",
+      desc: "UNESCO Almaty",
+    },
+    points: [
+      "Workshop conclusions, way forward and next steps – UNESCO and UZH",
+    ],
+  },
+}
+
+export const DayThree = {
+  partOne: {
+    title: "Day 3: Opening Session",
+    chair: {
+      name: "Ms Kristine Tovmasyan",
+      desc: "UNESCO Almaty",
+    },
+    points: [
+      "Welcoming remarks from the Chair (10 min)",
+      "Adoption of the meeting agenda and the composition of the Project Steering Committee (10 min)",
+    ],
+  },
+  partTwo: {
+    title: "Plenary Discussions ",
+    points: [
+      "Overview of the GLOFCA project Year 1 work plan and progress on activities’ implementation. Approval of the Year 1 work plan (20 min) – Natalya Kim, UNESCO Almaty",
+      "Discussion of the change of the project pilot sites in Kyrgyzstan (15 min)",
+      "Overview and adoption of the Project Grievance Mechanism (15 min) – Natalya Kim, UNESCO Almaty",
+      "AOB (next PSC, etc.)",
+    ],
+  },
+}
--- src/pages/tashkent-conference-2021.js
+++ src/pages/tashkent-conference-2021.js
@@ -1,6 +1,11 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
+// data
+import { DayOne, DayTwo, DayThree } from "../data/tashkent2021"
+
+// components
 import {
   Member,
   Section,
@@ -8,35 +13,209 @@ import {
   CTAAreaTwoCol,
   Blurb,
   VideoPlayer,
+  Hero,
 } from "../components"
 import Layout from "../components/Layout"
 
+const TalkSection = ({ title, chair, chairs, speakers, points }) => (
+  <>
+    <h4 className="text__coloured">{title}</h4>
+    {chair && (
+      <p>
+        <a
+          href={DayOne.partOne.chair.link}
+          target="_blank"
+          className="text__italicised"
+        >
+          {DayOne.partOne.chair.name}
+        </a>
+        , {DayOne.partOne.chair.desc}
+      </p>
+    )}
+    {chairs && (
+      <p>
+        Co-chairs:{" "}
+        {chairs.map((chair, i) => {
+          const { name, desc } = chair
+          return (
+            <span key={i} className="text__italicised">
+              {name}, {desc}
+              {", "}
+            </span>
+          )
+        })}
+      </p>
+    )}
+    {speakers &&
+      speakers.map((speaker, i) => {
+        return (
+          <p>
+            <a
+              key={i}
+              href={speaker.link}
+              target="_blank"
+              className="text__coloured"
+            >
+              {speaker.name}
+            </a>
+            , {speaker.desc}
+          </p>
+        )
+      })}
+    {points && (
+      <ul className="discs">
+        {points.map((point, i) => {
+          return (
+            <li key={i} className="disc">
+              {point}
+            </li>
+          )
+        })}
+      </ul>
+    )}
+  </>
+)
+
 const TashkentConference2021 = ({ data }) => {
   const participants = data.allWpTeamMember.nodes
-  const aboutText = data.about.tashkentConference2021
-  console.log(aboutText)
+  const coverImage = data.about.tashkentConference2021.coverImage.localFile
+  const video = data.about.tashkentConference2021.video
+  const aboutTextArray = [
+    {
+      title: data.about.tashkentConference2021.aboutTitleA,
+      text: data.about.tashkentConference2021.aboutTextA,
+    },
+    {
+      title: data.about.tashkentConference2021.aboutTitleB,
+      text: data.about.tashkentConference2021.aboutTextB,
+    },
+    {
+      title: data.about.tashkentConference2021.aboutTitleC,
+      text: data.about.tashkentConference2021.aboutTextC,
+    },
+  ]
+  const sponsors = data.about.tashkentOrganisersAndSponsors
+  const sponsorsArray = [
+    sponsors.one.localFile,
+    sponsors.two.localFile,
+    sponsors.three.localFile,
+    sponsors.four.localFile,
+    sponsors.five.localFile,
+    sponsors.six.localFile,
+  ]
   return (
     <Layout>
-      <Section title="About">
-        <SectionContent>
-          <div className="tashkent-about">
-            <div className="tashkent-about--text-box">
-              <h4>{aboutText.aboutTitleA}</h4>
-              <p>{aboutText.aboutTextA}</p>
-            </div>
-            <div className="tashkent-about--text-box">
-              <h4>{aboutText.aboutTitleB}</h4>
-              <p>{aboutText.aboutTextB}</p>
-            </div>
-            <div className="tashkent-about--text-box">
-              <h4>{aboutText.aboutTitleC}</h4>
-              <p>{aboutText.aboutTextC}</p>
-            </div>
-          </div>
+      <Hero
+        image={coverImage}
+        content="Reducing Vulnerabilities of Populations in the Central Asia Region from Glacier Lake Outburst Floods (GLOF) in a changing climate"
+        subText="UNESCO-Adaptation Fund GLOFCA First Regional Exchange Workshop / 18-19 November 2021"
+        height="full"
+      />
+      <Section title="Organisers & Sponsors">
+        <SectionContent customClass="tashkent__sponsors">
+          {sponsorsArray.map((sponsor, i) => {
+            console.log(sponsor)
+            return (
+              <div>
+                <GatsbyImage
+                  image={getImage(sponsor)}
+                  className="tashkent__sponsor"
+                />
+              </div>
+            )
+          })}
+        </SectionContent>
+      </Section>
+      <Section title="About" customClass="tashkent-section__image">
+        <SectionContent customClass="tashkent-about">
+          {aboutTextArray.map((text, i) => {
+            return (
+              <div key={i} className="tashkent-about--text-box">
+                <h5 className="text__coloured">{text.title}</h5>
+                <p>{text.text}</p>
+              </div>
+            )
+          })}
         </SectionContent>
       </Section>
       <Section customClass="purple-bg" title="Talks & Presentations">
-        <SectionContent></SectionContent>
+        <SectionContent>
+          <div className="tashkent-talks">
+            <div className="tashkent-talk talk-a">
+              <TalkSection
+                title={DayOne.partOne.title}
+                chair={DayOne.partOne.chair}
+                speakers={DayOne.partOne.speakers}
+              />
+              <TalkSection
+                title={DayOne.overview.title}
+                speakers={DayOne.overview.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionOne.title}
+                chairs={DayOne.sessionOne.chairs}
+                speakers={DayOne.sessionOne.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionTwo.title}
+                chairs={DayOne.sessionTwo.chairs}
+                speakers={DayOne.sessionTwo.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionThree.title}
+                chairs={DayOne.sessionThree.chairs}
+                speakers={DayOne.sessionThree.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionFour.title}
+                chairs={DayOne.sessionFour.chairs}
+                speakers={DayOne.sessionFour.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionFive.title}
+                chair={DayOne.sessionFive.chair}
+                points={DayOne.sessionFive.points}
+              />
+              <TalkSection
+                title={DayOne.sessionSix.title}
+                chair={DayOne.sessionSix.chair}
+                speakers={DayOne.sessionSix.speakers}
+              />
+              <TalkSection
+                title={DayOne.sessionSeven.title}
+                chair={DayOne.sessionSeven.chair}
+              />
+            </div>
+            <div className="tashkent-talk talk-b">
+              <TalkSection
+                title={DayTwo.partOne.title}
+                chair={DayTwo.partOne.chair}
+                points={DayTwo.partOne.points}
+              />
+              <TalkSection
+                title={DayTwo.partTwo.title}
+                chair={DayTwo.partTwo.chair}
+                points={DayTwo.partTwo.points}
+              />
+              <TalkSection
+                title={DayTwo.partThree.title}
+                chair={DayTwo.partThree.chair}
+                points={DayTwo.partThree.points}
+              />
+            </div>
+            <div className="tashkent-talk talk-c">
+              <TalkSection
+                title={DayThree.partOne.title}
+                chair={DayThree.partOne.chair}
+                points={DayThree.partOne.points}
+              />
+              <TalkSection
+                title={DayThree.partTwo.title}
+                points={DayThree.partTwo.points}
+              />
+            </div>
+          </div>
+        </SectionContent>
       </Section>
       <Section customClass="team" title="Participants">
         <SectionContent customClass="team-center">
@@ -60,10 +239,10 @@ const TashkentConference2021 = ({ data }) => {
         </SectionContent>
       </Section>
       <CTAAreaTwoCol
-        leftColumn={<Blurb subHeading="Watch and Download" />}
+        leftColumn={<Blurb subHeading="Watch" />}
         rightColumn={
           <VideoPlayer
-            videoURL="https://youtu.be/gtuwnJadS_4"
+            videoURL={video}
             customClass="videoplayer--shadow videoplayer--cta"
           />
         }
@@ -107,6 +286,58 @@ export const query = graphql`
         aboutTitleA
         aboutTitleB
         aboutTitleC
+        video
+        coverImage {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+      }
+      tashkentOrganisersAndSponsors {
+        one {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        two {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        three {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        four {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        five {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        six {
+          localFile {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
       }
     }
   }
